5c9424332aac2b83aeb2f5a9c818c25f6dfa7ad1
SamirMamede <anuarsamir@gmail.com>
2022-07-06 20:08:52 -0400

    create toolbar and module curriculum

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'curriculum'},
+  {
+    path: 'curriculum',
+    loadChildren: () => import('./curriculum/curriculum.module').then(m => m.CurriculumModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
