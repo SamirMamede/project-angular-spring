2f426552a9ca6eda46b50e21f7539ecf21d72bbd
SamirMamede <anuarsamir@gmail.com>
2022-07-17 00:25:44 -0400

    connecting front and back

--- crud-angular/src/app/curriculum/services/curriculum.service.ts
+++ crud-angular/src/app/curriculum/services/curriculum.service.ts
@@ -3,19 +3,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Curriculum } from '../model/curriculum';
-import { delay, first, tap } from 'rxjs';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurriculumService {
 
-  private readonly API = '/assets/curriculum.json';
+  private readonly API = 'api/curriculums';
 
   constructor(private httpClient: HttpClient) { }
 
   list() {
-    return   this.httpClient.get<Curriculum[]>(this.API).pipe(
+    return   this.httpClient.get<Curriculum[]>(this.API)
+    .pipe(
       first(),
       tap(curriculum => console.log(curriculum))
     );
